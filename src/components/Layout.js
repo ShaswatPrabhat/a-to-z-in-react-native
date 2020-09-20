9178d85533b6ec836b66456db93409bd3f7ab671
Shaswat Prabhat <spd1238@gmail.com>
2020-09-20 15:19:56 +0530

    Shaswat: Add react helmet for rendering headers and meta tags.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {useSiteMetadata} from '../hooks/useSiteMetadata';
 import styled from 'styled-components';
 import {Header} from './Header';
+import {Helmet} from "react-helmet";
 
 const AppStyles = styled.main`
   width: 800px;
@@ -12,6 +13,13 @@ export const Layout = ({children}) => {
     const {title, description} = useSiteMetadata();
     return (
         <AppStyles>
+            <Helmet>
+                <meta charSet="utf-8"/>
+                <meta name="author" content="Shaswat Prabhat, Nikhil Ribeiro and Aditya Saxena"/>
+                <meta name="description"
+                      content="An a to z in react-native, topic, code nuggets and challenges in react-native development"/>
+                <title>A to Z in react-native</title>
+            </Helmet>
             <Header siteTitle={title} siteDescription={description}/>
             {children}
         </AppStyles>
