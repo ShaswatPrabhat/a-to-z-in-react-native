e5da0cc1beb62acc7137c07fa24d647560a7b981
Shaswat Prabhat <spd1238@gmail.com>
2020-09-21 13:49:06 +0530

    Shaswat: Cherry pick basic style from Lincoln theme.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,20 @@
 import Typography from "typography"
-import oceanBeachTheme from 'typography-theme-lincoln'
-
-const typography = new Typography(oceanBeachTheme)
+const typography = new Typography({
+    baseFontSize: "19px",
+    baseLineHeight: 1.58,
+    googleFonts: [
+        {
+            name: "Varela Round",
+            styles: ["400"],
+        },
+        {
+            name: "Lora",
+            styles: ["400", "400i", "700"],
+        },
+    ],
+    bodyFontFamily: ["Lora", "serif"],
+    headerWeight: "400",
+    bodyWeight: 400,
+    boldWeight: 700,
+})
 export default typography
