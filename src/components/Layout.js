9e12a17a6ffca1574f3ba182fb3b046d5f171799
Shaswat Prabhat <spd1238@gmail.com>
2020-09-22 22:43:36 +0530

    Shaswat: Add GradientWrapper to Layout.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,18 +4,23 @@ import styled from 'styled-components';
 import {Header} from './Header';
 import {Meta} from "./Meta";
 
+const GradientWrapper = styled.html`
+    height: '100%';
+`;
 const AppStyles = styled.main`
-  width: 800px;
-  margin: 0 auto;
+    width: 800px;
+    margin: 0 auto;
 `;
 
 export const Layout = ({children}) => {
     const {title, description} = useSiteMetadata();
     return (
-        <AppStyles>
-            <Meta/>
-            <Header siteTitle={title} siteDescription={description}/>
-            {children}
-        </AppStyles>
+        <GradientWrapper>
+            <AppStyles>
+                <Meta/>
+                <Header siteTitle={title} siteDescription={description}/>
+                {children}
+            </AppStyles>
+        </GradientWrapper>
     );
 };
