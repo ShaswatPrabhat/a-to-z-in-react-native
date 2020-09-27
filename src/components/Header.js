4606934e31091636b36cc9cfea986f4f7bfdd058
Shaswat Prabhat <spd1238@gmail.com>
2020-09-27 16:34:38 +0530

    Shaswat: Add asets for theme change.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,33 @@ import React, { useContext } from "react";
 import ThemeContext from "../themeProvider/context";
 
 export const Header = ({ siteTitle, siteDescription }) => {
-  const { themeToggle } = useContext(ThemeContext);
+  const { theme, themeToggle } = useContext(ThemeContext);
   return (
     <>
-      <button style={{ height: 60, width: 120 }} onClick={themeToggle}>
-        <text>Toggle Theme</text>
+      <button
+        style={{
+          height: 60,
+          width: 60,
+          backgroundColor: "transparent",
+          border: "none",
+        }}
+        onClick={(event) => {
+            console.warn(event);
+          themeToggle();
+        }}
+      >
+        <img
+          src={
+            theme === "dark"
+              ? require("../assets/sun-regular.svg")
+              : require("../assets/moon-solid.svg")
+          }
+          alt="Change theme"
+          style={{
+            marginBottom: -20,
+            backgroundColor: "white",
+          }}
+        />
       </button>
       <Link to="/">
         <h1>{siteTitle}</h1>
